9c9f203beb26819bf77192839f8aa2c73cc617cb
julie <juliekim8312@gmail.com>
2019-04-27 19:25:12 +0900

    Add result table

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -3,17 +3,22 @@ package com.example.android.quizapp;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int TOTAL_QUESTION_NUMBERS = 4;
     private static final String QUESTION_NUMBER_2_ANSWER = "15";
+
+    private SparseBooleanArray mQuizResult = new SparseBooleanArray();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,21 +71,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onSubmitButtonClick(View view) {
+        mQuizResult.append(getArrayIndex(1), isQuestionNumberOneCorrect());
+        mQuizResult.append(getArrayIndex(2), isQuestionNumberTwoCorrect());
+        mQuizResult.append(getArrayIndex(3), isQuestionNumberThreeCorrect());
+        mQuizResult.append(getArrayIndex(4), isQuestionNumberFourCorrect());
+
+        showResultMessage(getNumberOfCorrectAnswer());
+        showResultTable();
+    }
+
+    private int getArrayIndex(int questionNumber) {
+        return questionNumber - 1;
+    }
+
+    private int getNumberOfCorrectAnswer() {
         int correctNumbers = 0;
-        if (isQuestionNumberOneCorrect()) {
-            correctNumbers++;
-        }
-        if (isQuestionNumberTwoCorrect()) {
-            correctNumbers++;
-        }
-        if (isQuestionNumberThreeCorrect()) {
-            correctNumbers++;
-        }
-        if (isQuestionNumberFourCorrect()) {
-            correctNumbers++;
+        for (int i = 0; i < mQuizResult.size(); i++) {
+            if (mQuizResult.valueAt(i)) {
+                correctNumbers++;
+            }
         }
 
-        showResultMessage(correctNumbers);
+        return correctNumbers;
     }
 
     private void showResultMessage(int correctNumbers) {
@@ -91,5 +103,36 @@ public class MainActivity extends AppCompatActivity {
         message += getString(R.string.correct_number, correctNumbers);
 
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    private void showResultTable() {
+        TextView resultNumberOne = findViewById(R.id.result_01);
+        resultNumberOne.setText(getResultTextResId(mQuizResult.valueAt(getArrayIndex(1))));
+
+        TextView resultNumberTwo = findViewById(R.id.result_02);
+        resultNumberTwo.setText(getResultTextResId(mQuizResult.valueAt(getArrayIndex(2))));
+
+        TextView resultNumberThree = findViewById(R.id.result_03);
+        resultNumberThree.setText(getResultTextResId(mQuizResult.valueAt(getArrayIndex(3))));
+
+        TextView resultNumberFour = findViewById(R.id.result_04);
+        resultNumberFour.setText(getResultTextResId(mQuizResult.valueAt(getArrayIndex(4))));
+
+        findViewById(R.id.result_table).setVisibility(View.VISIBLE);
+
+        scrollToBottom();
+    }
+
+    private int getResultTextResId(boolean correct) {
+        return correct ? R.string.result_o : R.string.result_x;
+    }
+
+    private void scrollToBottom() {
+        final ScrollView scrollView = findViewById(R.id.scroll_view);
+        scrollView.post(new Runnable() {
+            public void run() {
+                scrollView.smoothScrollTo(0, scrollView.getBottom());
+            }
+        });
     }
 }
